86ac16b7ed4ffdeaebf2b5f0518d65b375e3874f
LemonATsu <AtSu@AtSudeMacBook-Pro.local>
2015-07-04 22:28:22 +0800

    Happiness: Lecture 5 demo

---
+++ Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by AtSu on 2015/7/4.
+//  Copyright (c) 2015年 AtSu. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+
+}
